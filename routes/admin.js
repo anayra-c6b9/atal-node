2939e12f42693d7679b24ab49677c20157f55e96
anayra-c6b9 <anayra13@proton.me>
2024-05-30 21:12:55 +0530

    get to post in verify

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import {
-  checkToken,
-  deleteParticipantById,
-  getEventParticipants,
-  getParticipantById,
+    checkToken,
+    deleteParticipantById,
+    getEventParticipants,
+    getParticipantById,
 } from "../controller/admin.js";
 
 const adminRouter = Router();
 
 adminRouter.post("/admin/participant-by-event", getEventParticipants);
 adminRouter.post("/admin/participant-by-id", getParticipantById);
-adminRouter.get("/admin/verify", checkToken)
-adminRouter.post("/admin/delete-participant-by-id", deleteParticipantById)
+adminRouter.post("/admin/verify", checkToken);
+adminRouter.post("/admin/delete-participant-by-id", deleteParticipantById);
 export { adminRouter };
